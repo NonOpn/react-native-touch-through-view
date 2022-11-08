dc5bb4b37a4637a9811565fba11aabd6bd2bdd0f
Kévin Le Perf <codlabtech@gmail.com>
2022-11-08 02:11:59 +0100

    bump to npmjs version

--- android/src/main/java/com/rome2rio/android/reactnativetouchthroughview/TouchThroughViewPackage.java
+++ android/src/main/java/com/rome2rio/android/reactnativetouchthroughview/TouchThroughViewPackage.java
@@ -3,6 +3,7 @@ package com.rome2rio.android.reactnativetouchthroughview;
 import android.app.Activity;
 
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
@@ -20,6 +21,10 @@ public class TouchThroughViewPackage implements ReactPackage {
 
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 
